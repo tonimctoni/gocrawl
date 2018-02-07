85e7f9478d9810ff8cac6971122c4e50b7308807
tonimctoni <nicoflam@outlook.com>
2018-02-07 02:54:18 +0100

    removed a mistake

--- StringReservoir_test.go
+++ StringReservoir_test.go
@@ -4,67 +4,67 @@ import "testing"
 
 func TestStringReservoir(t *testing.T) {
     string_reservoir:=NewStringReservoir(10)
-    if len(string_reservoir.strings)!=0 || string_reservoir.capacity!=10{
+    if len(string_reservoir.strings)!=0 || cap(string_reservoir.strings)!=10{
         t.Fail()
     }
     string_reservoir.add("hello")
-    if len(string_reservoir.strings)!=1 || string_reservoir.capacity!=10{
+    if len(string_reservoir.strings)!=1 || cap(string_reservoir.strings)!=10{
         t.Fail()
     }
     s,e:=string_reservoir.get()
-    if s!="hello" || e!=nil || len(string_reservoir.strings)!=0 || string_reservoir.capacity!=10{
+    if s!="hello" || e!=nil || len(string_reservoir.strings)!=0 || cap(string_reservoir.strings)!=10{
         t.Fail()
     }
     s,e=string_reservoir.get()
-    if s!="" || e==nil || len(string_reservoir.strings)!=0 || string_reservoir.capacity!=10{
+    if s!="" || e==nil || len(string_reservoir.strings)!=0 || cap(string_reservoir.strings)!=10{
         t.Fail()
     }
 
     for i:=0;i<12;i++{
         string_reservoir.add("hello")
     }
-    if len(string_reservoir.strings)!=10 || string_reservoir.capacity!=10{
+    if len(string_reservoir.strings)!=10 || cap(string_reservoir.strings)!=10{
         t.Fail()
     }
 
     for i:=0;i<10;i++{
         s,e:=string_reservoir.get()
-        if s!="hello" || e!=nil || len(string_reservoir.strings)!=10-i-1 || string_reservoir.capacity!=10{
+        if s!="hello" || e!=nil || len(string_reservoir.strings)!=10-i-1 || cap(string_reservoir.strings)!=10{
             t.Fail()
         }
     }
     s,e=string_reservoir.get()
-    if s!="" || e==nil || len(string_reservoir.strings)!=0 || string_reservoir.capacity!=10{
+    if s!="" || e==nil || len(string_reservoir.strings)!=0 || cap(string_reservoir.strings)!=10{
         t.Fail()
     }
 
     string_reservoir.add_slice([]string{"hello","hello","hello"})
-    if len(string_reservoir.strings)!=3 || string_reservoir.capacity!=10{
+    if len(string_reservoir.strings)!=3 || cap(string_reservoir.strings)!=10{
         t.Fail()
     }
 
     s,e=string_reservoir.get()
-    if s!="hello" || e!=nil || len(string_reservoir.strings)!=2 || string_reservoir.capacity!=10{
+    if s!="hello" || e!=nil || len(string_reservoir.strings)!=2 || cap(string_reservoir.strings)!=10{
         t.Fail()
     }
 
     string_reservoir.add_slice([]string{"hello","hello","hello"})
-    if len(string_reservoir.strings)!=5 || string_reservoir.capacity!=10{
+    if len(string_reservoir.strings)!=5 || cap(string_reservoir.strings)!=10{
         t.Fail()
     }
 
     string_reservoir.add_slice([]string{"hello","hello","hello"})
-    if len(string_reservoir.strings)!=8 || string_reservoir.capacity!=10{
+    if len(string_reservoir.strings)!=8 || cap(string_reservoir.strings)!=10{
         t.Fail()
     }
 
     string_reservoir.add_slice([]string{"hello","hello","hello"})
-    if len(string_reservoir.strings)!=10 || string_reservoir.capacity!=10{
+    if len(string_reservoir.strings)!=10 || cap(string_reservoir.strings)!=10{
         t.Fail()
     }
 
     s,e=string_reservoir.get()
-    if s!="hello" || e!=nil || len(string_reservoir.strings)!=9 || string_reservoir.capacity!=10{
+    if s!="hello" || e!=nil || len(string_reservoir.strings)!=9 || cap(string_reservoir.strings)!=10{
         t.Fail()
     }
 }
--- makefile
+++ makefile
@@ -1,8 +1,8 @@
 all:
-	go build gocrawl.go Client.go UrlFinder.go CssManager.go Sets.go StringReservoir.go
+	/usr/local/go/bin/go build gocrawl.go Client.go UrlFinder.go CssManager.go Sets.go StringReservoir.go
 run:
-	go run gocrawl.go Client.go UrlFinder.go CssManager.go Sets.go StringReservoir.go
+	/usr/local/go/bin/go run gocrawl.go Client.go UrlFinder.go CssManager.go Sets.go StringReservoir.go
 test:
-	go test gocrawl.go Client.go UrlFinder.go CssManager.go Sets.go StringReservoir.go StringReservoir_test.go Sets_test.go
+	/usr/local/go/bin/go test gocrawl.go Client.go UrlFinder.go CssManager.go Sets.go StringReservoir.go StringReservoir_test.go Sets_test.go
 
 # go build -ldflags "-s -w"
